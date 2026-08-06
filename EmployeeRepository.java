535f3a01ac4250d8d6c9ab8172dfba715ec9d0ba
Suraj Mohajkar <surajmohajkar@gmail.com>
2026-08-06 18:01:21 +0530

    EMS-001: Create Employee entity and status enum

---
+++ EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.suraj.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.suraj.ems.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
