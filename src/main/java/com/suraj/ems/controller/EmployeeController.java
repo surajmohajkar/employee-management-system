8832b1c54c04a2dc48c66be723e1b74ac8606400
Suraj Mohajkar <surajmohajkar@gmail.com>
2026-08-14 18:01:37 +0530

    EMS-007: Add employee request validation

--- src/main/java/com/suraj/ems/controller/EmployeeController.java
+++ src/main/java/com/suraj/ems/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.suraj.ems.controller;
 
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +11,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.suraj.ems.dto.EmployeeRequestDTO;
 import com.suraj.ems.dto.EmployeeResponseDTO;
 import com.suraj.ems.service.EmployeeService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
 	private final EmployeeService employeeService;
-	
+	   
 	public EmployeeController(EmployeeService employeeService) {
 		this.employeeService = employeeService;
 	}
@@ -29,7 +28,7 @@ public class EmployeeController {
 	//Create Employee
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public EmployeeResponseDTO saveEmployee(@RequestBody EmployeeRequestDTO requestDTO) {
+	public EmployeeResponseDTO saveEmployee(@Valid @RequestBody EmployeeRequestDTO requestDTO) {
 		return employeeService.saveEmployee(requestDTO);
 	}
 	
@@ -47,7 +46,7 @@ public class EmployeeController {
 	
 	//Update Employee
 	@PutMapping("/{id}")
-    public EmployeeResponseDTO updateEmployee(@PathVariable Long id,@RequestBody EmployeeRequestDTO requestDTO) {
+    public EmployeeResponseDTO updateEmployee(@PathVariable Long id, @Valid @RequestBody EmployeeRequestDTO requestDTO) {
         return employeeService.updateEmployee(id, requestDTO);
     }
 	
